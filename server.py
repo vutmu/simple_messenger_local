130358c955a16697085773b308d00f7a4e14f4a8
vutmu <gabitov1983@gmail.com>
2020-09-22 01:03:02 +0300

    functions /send and /status works with DB

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import psycopg2
 from flask import Flask, request, abort
 import time
 
@@ -12,9 +13,10 @@ def hello():
 
 @app.route("/status")
 def status():
-    members_count = len(set(i['name'] for i in db))
+    messages_count = pgdb('SELECT max(id) FROM messages;')[-1][-1]
+    members_count = pgdb('SELECT count(distinct name) FROM messages;')[-1][-1]
     return {'members': members_count,
-            'messages_count': len(db),
+            'messages_count': messages_count,
             'Status': 'ok'}
 
 
@@ -24,28 +26,31 @@ def messenger():
         after_timestamp = float(request.args['after_timestamp'])
     else:
         after_timestamp = 0
-    max_limit=100
+    max_limit = 100
     if 'limit' in request.args:
-        limit=int(request.args['limit'])
-        if limit>max_limit:
-            abort(400,'too big limit')
+        limit = int(request.args['limit'])
+        if limit > max_limit:
+            abort(400, 'too big limit')
     else:
-        limit=max_limit
+        limit = max_limit
 
-    after_id=0
+    after_id = 0
     for i in db:
-        if i['timestamp']>after_timestamp:
+        if i['timestamp'] > after_timestamp:
             break
         else:
-            after_id+=1
-    return {'messages': db[after_id:after_id+limit]} #возвращает словарь {'messages': срез из базы данных}
+            after_id += 1
+    return {'messages': db[after_id:after_id + limit]}  # возвращает словарь {'messages': срез из базы данных}
 
 
-@app.route("/send", methods=['GET','POST']) #получает запрос типа пост из button_pressed месенджера
+@app.route("/send", methods=['GET', 'POST'])  # получает запрос типа пост из button_pressed месенджера
 def send():
     try:
         data = request.json
-        db.append({'id': len(db), # добавляет в БД новый словарь на основе полученного джейсон
+        query=(data['name'],data['text'],time.time())
+        query=f"INSERT INTO messages (name, message, posting_time) VALUES {query}"
+        pgdb(query)
+        db.append({'id': len(db),  # добавляет в БД новый словарь на основе полученного джейсон
                    'name': data['name'],
                    'text': data['text'],
                    'timestamp': time.time()})
@@ -53,6 +58,26 @@ def send():
     except:
         return 'Все норм'
 
+
+"""нужна функция, которая обращается к базе данных. Она получает запрос или генерирует его из
+входящих параметров и возвращает список кортежей."""
+
+
+def pgdb(query):
+    connection = psycopg2.connect(
+        database='simple_messenger',
+        user='force',
+        password='12345',
+        host='localhost',
+        port='5432',
+    )
+    connection.autocommit = True
+    cursor = connection.cursor()
+    cursor.execute(query)
+    try:
+        return cursor.fetchall()
+    except:
+        pass
 
 
 app.run()
